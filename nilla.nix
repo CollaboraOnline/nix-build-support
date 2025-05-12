548c8879430651dea06dc305140429dd1272d00e
Skyler Grey <skyler.grey@collabora.com>
2025-05-12 13:45:57 +0000

    init: add nix build support
    The following I know to work
    - building Core and Collabora Online for Android on NixOS
    - building Core and Collabora Online on NixOS
    - running Core and Collabora Online on NixOS, provided you also use this
      patch on Collabora Online sources (without it, you will fail to find
      fonts/etc.)
    
    ```diff
    diff --git a/coolwsd-systemplate-setup b/coolwsd-systemplate-setup
    index 8d60157d23..f4b9fc26bb 100755
    --- a/coolwsd-systemplate-setup
    +++ b/coolwsd-systemplate-setup
    @@ -20,9 +20,9 @@
     # while INSTDIR is the physical one. Both will most likely be the same,
     # except on systems that have symlinks in the path. We must create
     # both paths (if they are different) inside the jail, hence we need both.
    -CHROOT=`cd $CHROOT && /bin/pwd`
    -INSTDIR_LOGICAL=`cd $INSTDIR && /bin/pwd -L`
    -INSTDIR=`cd $INSTDIR && /bin/pwd -P`
    +CHROOT=`cd $CHROOT && pwd`
    +INSTDIR_LOGICAL=`cd $INSTDIR && pwd -L`
    +INSTDIR=`cd $INSTDIR && pwd -P`
    
     if [ ! `uname -s` = "FreeBSD" ]; then
         CP=cp
    @@ -157,4 +157,9 @@
         test -d $HOME/.fonts && ${CP} -r -p -L $HOME/.fonts $CHROOT/$HOME
     fi
    
    +if test -d /nix; then
    +    mkdir -p $CHROOT/nix
    +    sudo mount --bind /nix $CHROOT/nix
    +fi
    +
     exit 0
    ```
    
    you will probably want to keep this directory *alongside* your nix
    rather than cloning into it. That's because nilla *will copy everything
    to your store if you use it to enter a shell, leading to a very long
    delay*. LibreOffice translations/etc. are gigabytes big in a full clone.
    You do not want that.
    
    ---
    
    The .envrc.template therefore assumes that you have done this, and
    further that the folder you have is called 'nilla'. To do that, you'll
    need to clone this repository with the following
    
        git clone git@github.com:Minion3665/collabora--nix-build-support nilla
    
    The .envrc.template also assumes you have some nested directory
    structure, such as
    
        - nilla/
        - android/
          - core/
          - online/
        - standard/
          - core/
          - online/
    
    and are placing it in your clone directories (here 'core' and 'online')
    
    This is a reasonable assumption as you will need separate shells for
    core and online, so you can't just place the .envrc directly above your
    clone of this repository.
    
    If you decide to change either of these assumptions, the path should be
    trivial to modify. You'll need to change the envrc anyway to fill in the
    shell name you want. Again, this should be fairly self explanatory

---
+++ nilla.nix
@@ -0,0 +1,302 @@
+let
+  pins = import ./npins;
+
+  nilla = import pins.nilla;
+
+  settings = {
+    nixpkgs.configuration = {
+      android_sdk.accept_license = true;
+      allowUnfree = true;
+      allowBroken = true;
+    };
+  };
+in nilla.create ({ config }: {
+  config = let
+    coreNdkVersion = "27.2.12479018";
+    onlineNdkVersion = "27.2.12479018";
+    pocoNdkVersion = "27.2.12479018";
+    zstdNdkVersion = "27.2.12479018";
+    minPlatformVersion = "33"; # https://endoflife.date/android
+    abi = "arm64-v8a";
+    mkAndroidSdk = { androidenv, ndkVersion, cmakeVersion ? "latest", buildToolsVersion ? "latest", includeSources ? false, platformToolsVersion ? "latest" }: androidenv.composeAndroidPackages {
+      inherit minPlatformVersion includeSources platformToolsVersion;
+      abiVersions = [ abi ];
+      includeNDK = true;
+      ndkVersions = [
+        ndkVersion
+      ];
+      cmakeVersions = [
+        cmakeVersion
+      ];
+      buildToolsVersions = [
+        buildToolsVersion
+      ];
+    };
+  in {
+    inputs = builtins.mapAttrs (name: value: {
+      src = value;
+      settings = settings.${name} or config.lib.constants.undefined;
+    }) pins;
+
+    lib.constants.undefined = config.lib.modules.when false {};
+
+    # TODO: it might be worth moving the autogen stuff in here so that rather than doing `./autogen.sh $(envsubst ../autogen.args)` we do something like `./autogen.sh $AUTOGEN_ARGS`
+    # that would allow us to drop the envsubst dependency and weird env variables, nicely share the autogen args along with the configs, and probably not be largely more convoluted
+    # plus we can probably commit this *somewhere* even if it's only Minion3665/collabora--nix-build-support or somesuch
+    # the other thing we could do is generate autogen.input with a shell script run in direnv...
+    # ...that would have an extra step (`$ generate-autogen-input` or equivalent) but it would be automatically handled and make building more seamless...
+
+    shells.core = {
+      systems = [ "x86_64-linux" ];
+
+      # Define our shell environment.
+      shell = { lib, mkShell, ccache, libreoffice-collabora, boost, bsh, ... }:
+        mkShell {
+          packages = [
+            ccache
+          ];
+
+          inputsFrom = [
+            libreoffice-collabora
+          ];
+
+          CCACHE_COMPRESS = "1";
+          BOOST_DEV_PATH = boost.dev;
+          BOOST_LIB_PATH = "${boost}/lib";
+          BEANSHELL_JAR_PATH = bsh;
+          TMPDIR = "/tmp";
+        };
+    };
+
+    packages.android-poco = {
+      systems = [ "x86_64-linux" ];
+
+      package = { stdenv, fetchFromGitHub, androidenv, nettools }: let # nettools is for 'hostname'... weird but ok whatever
+        version = "1.14.2";
+        androidSdk = (mkAndroidSdk {
+          inherit androidenv;
+          ndkVersion = pocoNdkVersion;
+        });
+        sdkPath = "${androidSdk.androidsdk}/libexec/android-sdk";
+        ndkPath = "${sdkPath}/ndk/${pocoNdkVersion}";
+        toolchainBinPath = "${ndkPath}/toolchains/llvm/prebuilt/linux-x86_64/bin";
+      in stdenv.mkDerivation {
+        pname = "android-poco";
+        inherit version;
+
+        src = config.inputs.poco.result;
+
+        patches = [
+          ./patches/android-poco/arm64-support.patch
+        ];
+        
+        configureFlags = [
+          "--config=Android"
+          "--prefix=${placeholder "out"}"
+          "--no-samples"
+          "--no-tests"
+          "--omit=ActiveRecord,Crypto,NetSSL_OpenSSL,Zip,Data,Data/SQLite,Data/ODBC,Data/MySQL,MongoDB,PDF,CppParser,PageCompiler,JWT,Prometheus,Redis"
+        ];
+
+        nativeBuildInputs = [
+          nettools
+        ];
+
+        buildPhase = ''
+          export PATH="$PATH:${toolchainBinPath}"
+          export SYSLIBS=-static-libstdc++
+          export ANDROID_ABI=${abi}
+
+          echo $PATH
+          make -sj12 \
+            CC=aarch64-linux-android${minPlatformVersion}-clang \
+            CXX=aarch64-linux-android${minPlatformVersion}-clang++ \
+            LIB='llvm-ar -cr' \
+            RANLIB=llvm-ranlib
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          make -sj12 ANDROID_ABI=${abi} install
+        '';
+      };
+    };
+
+    packages.android-zstd = {
+      systems = [ "x86_64-linux" ];
+
+      package = { stdenv, fetchFromGitHub, androidenv, cmake }: let
+        androidSdk = (mkAndroidSdk {
+          inherit androidenv;
+          ndkVersion = zstdNdkVersion;
+        });
+        sdkPath = "${androidSdk.androidsdk}/libexec/android-sdk";
+        ndkPath = "${sdkPath}/ndk/${zstdNdkVersion}";
+        toolchainBinPath = "${ndkPath}/toolchains/llvm/prebuilt/linux-x86_64/bin";
+      in stdenv.mkDerivation {
+        pname = "android-zstd";
+        version = "unstable-2025-05-12";
+
+        src = config.inputs.zstd.result;
+        
+        nativeBuildInputs = [
+          cmake
+        ];
+
+        dontConfigure = true;
+
+        buildPhase = ''
+          mkdir -p install/${abi}
+          cd install/${abi}
+
+          cmake ../../build/cmake \
+            -DCMAKE_TOOLCHAIN_FILE=${ndkPath}/build/cmake/android.toolchain.cmake \
+            -DANDROID_ABI=${abi} \
+            -DCMAKE_ANDROID_ARCH_ABI=${abi} \
+            -DANDROID_NDK=${ndkPath} \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_SYSTEM_NAME=Android \
+            -DCMAKE_SYSTEM_VERSION=${minPlatformVersion} \
+            -DZSTD_BUILD_PROGRAMS:BOOL=OFF \
+            -DZSTD_BUILD_SHARED:BOOL=OFF || exit 1;
+
+          make
+
+          cd ../..
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          mv lib $out/include
+          mv install/${abi} $out
+        '';
+      };
+    };
+
+    shells.android-core = {
+      systems = [ "x86_64-linux" ];
+
+      # Define our shell environment.
+      shell = { lib, clangStdenv, mkShell, androidenv, libreoffice-collabora, envsubst, ccache, ... }: let
+        androidSdk = (mkAndroidSdk {
+          inherit androidenv;
+          ndkVersion = coreNdkVersion;
+        });
+        sdkPath = "${androidSdk.androidsdk}/libexec/android-sdk";
+        ndkPath = "${sdkPath}/ndk/${coreNdkVersion}";
+        toolchainBinPath = "${ndkPath}/toolchains/llvm/prebuilt/linux-x86_64/bin";
+      in
+        mkShell.override {
+          stdenv = clangStdenv;
+          # Required as otherwise inner packages see that gcc is available, try to build with it, and fall flat on their faces
+          # We want them to default to the first clang in our path while LO can build some stuff with the other clangs
+          # This is the closest I can reasonably get to a "normal" setup where you add your ndk bin directory to your path before your regular cc ... but I think it's enough
+        } {
+          packages = [
+            envsubst
+            ccache
+          ];
+
+          inputsFrom = [
+            libreoffice-collabora
+          ];
+
+          CCACHE_COMPRESS = "1";
+          CCACHE_CPP2 = "1";
+          ANDROID_SDK_PATH = sdkPath;
+          ANDROID_NDK_PATH = ndkPath;
+          TMPDIR = "/tmp";
+
+          shellHook = ''
+            export PKG_CONFIG_PATH_x86_64_unknown_linux_gnu="$PKG_CONFIG_PATH"
+            unset CC CXX AR LD
+            export STRIP=llvm-strip
+            export PATH="$PATH:${toolchainBinPath}"
+          '';
+        };
+    };
+    
+    shells.online = {
+      # Cannot merge with core, as lo builds with python3.11+distutils and co builds with python3.12 without distutils
+      # TODO: it feels theoretically possible to set up something that has priorities such that python3.12 is ignored - at which point maybe we can use the same env(?)
+      # I'm suspicious of being able to use android-core along with core though - especially as clang stdenv is required for it, so maybe we need multiple shells
+      # or maybe that is just better handled with separate shells and direnv anyway(?)
+      systems = [ "x86_64-linux" ];
+
+      # Define our shell environment.
+      shell = { lib, mkShell, ccache, collabora-online, typescript-language-server, ... }:
+        mkShell {
+          packages = [
+            ccache
+            typescript-language-server
+          ];
+
+          inputsFrom = [
+            collabora-online
+          ];
+
+          CCACHE_COMPRESS = "1";
+          TMPDIR = "/tmp";
+        };
+    };
+    
+    shells.android-online = {
+      systems = [ "x86_64-linux" ];
+
+      # Define our shell environment.
+      shell = { lib, mkShell, ccache, collabora-online, android-studio, androidenv, jdk17, libgcc, jdt-language-server, typescript-language-server, chromium, ... }: let
+        androidSdk = (mkAndroidSdk {
+          inherit androidenv;
+          ndkVersion = onlineNdkVersion;
+          cmakeVersion = "3.22.1";
+          buildToolsVersion = "30.0.3";
+          platformToolsVersion = "34.0.5";
+          includeSources = true;
+        });
+        sdkPath = "${androidSdk.androidsdk}/libexec/android-sdk";
+        ndkPath = "${sdkPath}/ndk/${onlineNdkVersion}";
+        toolchainBinPath = "${ndkPath}/toolchains/llvm/prebuilt/linux-x86_64/bin";
+      in
+        mkShell {
+          packages = [
+            ccache
+            androidSdk.platform-tools
+            android-studio
+            jdk17
+            jdt-language-server
+            typescript-language-server
+            chromium
+          ];
+
+          inputsFrom = [
+            collabora-online
+          ];
+
+          CCACHE_COMPRESS = "1";
+          CCACHE_CPP2 = "1";
+          TMPDIR = "/tmp";
+          STUDIO_GRADLE_JDK = "${jdk17}/lib/openjdk";
+          ANDROID_HOME = sdkPath;
+          ANDROID_NDK_ROOT = ndkPath;
+
+          CONFIGURE_FLAGS = builtins.concatStringsSep " " [
+            "--enable-androidapp"
+            "--with-lo-builddir=/home/minion/Code/Collabora/android/core" # TODO: no way to fix this without some processing of this env var, I think...
+            "--with-poco-includes=${config.packages.android-poco.result.x86_64-linux}/include" # TODO: should I be using pkgs.system or somesuch here(?)
+            "--with-poco-libs=${config.packages.android-poco.result.x86_64-linux}/${abi}/lib"
+            "--with-zstd-includes=${config.packages.android-zstd.result.x86_64-linux}/include"
+            "--with-zstd-libs=${config.packages.android-zstd.result.x86_64-linux}/${abi}/lib"
+            "--enable-silent-rules"
+            "--enable-debug"
+            "--with-android-abi=${abi}"
+            "--disable-werror" # TODO: there are some warnings while building android about some redefined macros...
+          ];
+
+          shellHook = ''
+            export PATH="$PATH:${toolchainBinPath}"
+            export LD_LIBRARY_PATH="${libgcc}/lib:$LD_LIBRARY_PATH"
+          '';
+        };
+    };
+  };
+})
